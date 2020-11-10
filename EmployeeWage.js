bbbd11bdf968044034fa1f09520d095ecb088a95
pallavikyama <kyamap98@gmail.com>
2020-11-10 08:28:01 +0530

    Refactored: Used a function to get working hours based on work type and calculated the daily wage.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,23 +5,23 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 {
-    let empHours = 0;
+    function getWorkingHours(empCheck) {
+        switch (empCheck) {
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
+    }
     let empCheck = Math.floor(Math.random() * 10) % 3;
     if (empCheck == IS_ABSENT) {
         console.log("Employee is ABSENT.");
     } else {
         console.log("Employee is PRESENT.");
     }
-    switch (empCheck) {
-        case IS_PART_TIME:
-            empHours = PART_TIME_HOURS;
-            break;
-        case IS_FULL_TIME:
-            empHours = FULL_TIME_HOURS;
-            break;
-        default:
-            empHours = 0;
-    }
+    let empHours = getWorkingHours(empCheck);
     let empWage = empHours * WAGE_PER_HOUR;
-    console.log("Employee Wage:" + empWage);
+    console.log("Working Hours:" + empHours + "  Employee Wage:" + empWage);
 }
