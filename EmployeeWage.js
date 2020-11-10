b3624f31526c87792ac2ec887126bf6dd9402b81
pallavikyama <kyamap98@gmail.com>
2020-11-10 08:59:31 +0530

    Modified: Calulated Monthly wage till a condition of total working hours or max working days is reached using while loop.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,24 +5,25 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_WORKING_HOURS_IN_MONTH = 160;
 
-{
-    function getWorkingHours(empCheck) {
-        switch (empCheck) {
-            case IS_PART_TIME:
-                return PART_TIME_HOURS;
-            case IS_FULL_TIME:
-                return FULL_TIME_HOURS;
-            default:
-                return 0;
-        }
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
     }
-
-    let totalEmpHours = 0;
-    for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-        let empCheck = Math.floor(Math.random() * 10) % 3;
-        totalEmpHours += getWorkingHours(empCheck);
-    }
-    let empWage = totalEmpHours * WAGE_PER_HOUR;
-    console.log("Total Working Hours:" + totalEmpHours + "  Employee Wage:" + empWage);
 }
+
+let totalEmpHours = 0;
+let totalWorkingDays = 0;
+while (totalEmpHours <= MAX_WORKING_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHours += getWorkingHours(empCheck);
+}
+let empWage = totalEmpHours * WAGE_PER_HOUR;
+console.log("Total Working Days:" + totalWorkingDays + "  Total Working Hours:" + totalEmpHours + "  Employee Wage:" + empWage);
