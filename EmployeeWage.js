ca05ecde241855d3941fde236af1bf92eec81415
pallavikyama <kyamap98@gmail.com>
2020-11-10 08:17:32 +0530

    Added: Used Math.random to check employee attendance.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,7 @@
-console.log("Welcome to Employee Wage Computation Program!");
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+    console.log("Employee is ABSENT.");
+} else {
+    console.log("Employee is PRESENT.");
+}
