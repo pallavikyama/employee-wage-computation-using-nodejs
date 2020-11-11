511875d5475b5cf60acb7ab1bc132bcec5ac5e59
pallavikyama <kyamap98@gmail.com>
2020-11-11 07:44:23 +0530

    Added: Used Array helper functions to perform various array operations.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -18,7 +18,7 @@ function getWorkingHours(empCheck) {
     }
 }
 
-function calculateDailyWage(empHours) {
+function calculateWage(empHours) {
     return empHours * WAGE_PER_HOUR;
 }
 
@@ -30,8 +30,66 @@ while (totalEmpHours <= MAX_WORKING_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_
     let empCheck = Math.floor(Math.random() * 10) % 3;
     let empHours = getWorkingHours(empCheck);
     totalEmpHours += empHours;
-    empDailyWageArray.push(calculateDailyWage(empHours));
+    empDailyWageArray.push(calculateWage(empHours));
 }
-let empWage = calculateDailyWage(totalEmpHours);
-console.log("Total Working Days:" + totalWorkingDays + "  Total Working Hours:" + totalEmpHours + "  Employee Wage:" + empWage);
+let empWage = calculateWage(totalEmpHours);
 console.log("Employee Daily Wages for the whole month(array-format): " + empDailyWageArray);
+console.log("Total Working Days:" + totalWorkingDays + "  Total Working Hours:" + totalEmpHours + "  Total Employee Wage:" + empWage);
+
+// Array Helper Functions
+// Calculate Total Employee Wage using forEach traversal
+let totalEmpWage = 0;
+function sum(dailyWage) {
+    totalEmpWage += dailyWage;
+}
+empDailyWageArray.forEach(sum);
+console.log("UC-7-A1:Total Employee Wage (using forEach traversal): " + totalEmpWage);
+
+// Calculate Total Employee Wage using reduce method
+function totalWages(totalWage, dailyWage) {
+    return totalWage + dailyWage;
+}
+console.log("UC-7-A2:Total Employee Wage (using reduce method): " + empDailyWageArray.reduce(totalWages, 0));
+
+// Show Day along with Daily Wage using Array map helper function
+let dayCounter = 0;
+function mapDayWithWage(dailyWage) {
+    dayCounter++;
+    return dayCounter + " = " + dailyWage;
+}
+let mapDayWithWageArray = empDailyWageArray.map(mapDayWithWage);
+console.log("UC-7-B:Daily Wage Map:");
+console.log(mapDayWithWageArray);
+
+// Show Days when Full-time wage of '160' were earned
+function fullTimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+let fullDayWageArray = mapDayWithWageArray.filter(fullTimeWage);
+console.log("UC-7-C:Map of Daily Wages with filter when full-time wage is earned:");
+console.log(fullDayWageArray);
+
+// Find the first occurrence when full-time wage was earned (using find function)
+function findFulltimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+console.log("UC-7-D:First full-time wage was earned on day: " + mapDayWithWageArray.find(findFulltimeWage));
+
+// Check if Every element of fullDayWageArray is truly holding full-time wage
+function isAllFulltimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+console.log("UC-7-E:Check if all elements have full-time wage: " + fullDayWageArray.every(isAllFulltimeWage));
+
+// Check if there is any part-time wage in the array
+function isAnyParttimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+console.log("UC-7-F:Check if there is any part-time wage: " + mapDayWithWageArray.some(isAnyParttimeWage));
+
+// Find number of days the employee worked (or) was present
+function totalDaysWorked(numOfDays, dailyWage) {
+    if (dailyWage > 0) return ++numOfDays;
+    return numOfDays;
+}
+console.log("UC-7-G:Number of days Employee worked: " + empDailyWageArray.reduce(totalDaysWorked, 0));
